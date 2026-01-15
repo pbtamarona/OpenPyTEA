645677a1f4de6e61eeb6dca52c9464d08b46e630
Panji B. Tamarona <P.B.Tamarona@tudelft.nl>
2026-01-15 13:10:50 +0100

    updating __init__

--- src/openpytea/__init__.py
+++ src/openpytea/__init__.py
@@ -1,3 +1,24 @@
-import openpytea.plant
-import openpytea.equipment
-import openpytea.analysis
+from .plant import Plant
+from .equipment import Equipment
+from .analysis import (plot_direct_costs_bar, plot_fixed_capital_bar,
+                       plot_fixed_opex_bar, plot_variable_opex_bar,
+                       sensitivity_plot, tornado_plot, monte_carlo,
+                       plot_monte_carlo, plot_monte_carlo_inputs,
+                       plot_multiple_monte_carlo)
+
+
+# Define what gets exported when someone does `from openpytea import *`
+__all__ = [
+    "Plant",
+    "Equipment",
+    "plot_direct_costs_bar",
+    "plot_fixed_capital_bar",
+    "plot_fixed_opex_bar",
+    "plot_variable_opex_bar",
+    "sensitivity_plot",
+    "tornado_plot",
+    "monte_carlo",
+    "plot_monte_carlo",
+    "plot_monte_carlo_inputs",
+    "plot_multiple_monte_carlo",
+]
